7663882f99451fed4a103eaf64515d962bdf3e96
Innocent Manda <106418148+Chipatenii@users.noreply.github.com>
2024-05-01 00:32:11 +0200

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -8,4 +8,12 @@ exec { 'install_flask':
   command     => '/usr/bin/pip3 install Flask==2.1.0',
   path        => ['/usr/bin'],
   refreshonly => true,
+  notify      => Exec['verify_flask_installed'],
+}
+
+exec { 'verify_flask_installed':
+  command     => '/usr/local/bin/flask --version',
+  path        => ['/usr/local/bin'],
+  logoutput   => true,
+  refreshonly => true,
 }
