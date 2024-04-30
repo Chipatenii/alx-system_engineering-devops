a635547a7f58a8ea13eb30b8f13d3cdf168395c7
chipatenii <innocentmanda70@gmail.com>
2024-04-30 15:44:02 -0700

    po

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,19 +1,6 @@
-# Puppet manifest to install Flask version 2.1.0 using pip3
-
-package { 'python3-pip':
-  ensure => installed,
-}
-
-exec { 'install_flask':
-  command     => '/usr/bin/pip3 install Flask==2.1.0',
-  path        => ['/usr/bin'],
-  refreshonly => true,
-  notify      => Exec['verify_flask_installed'],
-}
-
-exec { 'verify_flask_installed':
-  command     => '/usr/local/bin/flask --version',
-  path        => ['/usr/local/bin'],
-  logoutput   => true,
-  refreshonly => true,
+# A puppet manifest installing flask v2.1.0 which is a package from pip3.
+# Am also specifying the version of flask to install using the ensure attribute
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => pip3,
 }
