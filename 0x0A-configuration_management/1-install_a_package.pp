a40a5c4d654513f0aaf822fe3d4efad17aeec640
Innocent Manda <106418148+Chipatenii@users.noreply.github.com>
2024-05-01 00:29:59 +0200

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,11 @@
-# Puppet manifest to install Flask using pip3
+# Puppet manifest to install Flask version 2.1.0 using pip3
 
-# Install Flask package from pip3
-package { 'Flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+package { 'python3-pip':
+  ensure => installed,
+}
+
+exec { 'install_flask':
+  command     => '/usr/bin/pip3 install Flask==2.1.0',
+  path        => ['/usr/bin'],
+  refreshonly => true,
 }
